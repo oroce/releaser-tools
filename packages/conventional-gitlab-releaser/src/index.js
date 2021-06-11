68b3646d72e4d1d475fbebbca9729a88e2182180
Róbert Oroszi <robert@oroszi.net>
2021-06-11 10:52:31 +0200

    Update index.js

--- packages/conventional-gitlab-releaser/src/index.js
+++ packages/conventional-gitlab-releaser/src/index.js
@@ -84,7 +84,7 @@ function conventionalGitlabReleaser (auth, changelogOpts, context, gitRawCommits
             }
           }
           debug(`posting %o to the following URL - ${url}`, options)
-        
+
           // Set auth after debug output so that we don't print auth token to console.
           options.token = auth.token
 
